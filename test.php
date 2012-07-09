be2cfaa3309bdffca536dcb4bb2e676185106316
Vladimir Chernyshev <volch5@gmail.com>
2012-07-09 17:14:19 +0400

    Pretty presentation

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
 <?php
 $expected = <<<'EOT'
 <h1>Пользователи</h1>
-<p>Username1</p><p>Username2</p><p>Username3</p><p>Username4</p>
+    <p>Username1</p>
+    <p>Username2</p>
+    <p>Username3</p>
+    <p>Username4</p>
+
 EOT;
 
 ob_start();
