f2e1aaffeac562fa5d407ffa2b520d0a23abf09a
Vladimir Chernyshev <volch5@gmail.com>
2012-07-09 16:56:24 +0400

    Divided logic to application and presentation.

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<h1>Пользователи</h1>
-<?
+<?php
+include 'DBConnector.php';
 $DB = new DBConnector;
-$DB->query(‘SELECT * FROM users LIMIT 10’);
+$DB->query('SELECT * FROM users LIMIT 10');
 if($DB->get_num_rows()){
     while($user = $DB->fetch_row()){
-        echo ‘<p>’.$user[‘name’].’</p>’;
+        $users[] = $user;
     }
+} else {
+    $users = array();
+}
+?>
+<h1>Пользователи</h1>
+<?
+foreach ($users as $user) {
+    echo '<p>'.$user['name'].'</p>';
 }
 ?>
