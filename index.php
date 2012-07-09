064d57435db1fbe2b288fe884991b588ad7ec604
Vladimir Chernyshev <volch5@gmail.com>
2012-07-09 17:33:41 +0400

    Revert "Extracted works with database to repository"
    This reverts commit 6327c8b0b9223aa9406622ae33baaa55c9a6cc87.

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-require_once 'DBConnector.php';
-require_once 'UserRepository.php';
-
-$user_repository = new UserRepository(new DBConnector);
-$users = $user_repository->getAll();
-
+include 'DBConnector.php';
+$DB = new DBConnector;
+$DB->query('SELECT * FROM users LIMIT 10');
+if($DB->get_num_rows()){
+    while($user = $DB->fetch_row()){
+        $users[] = $user;
+    }
+} else {
+    $users = array();
+}
 include 'index.php.html';
